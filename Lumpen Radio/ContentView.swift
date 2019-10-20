8153035b6a011f9e45c63440bc2594ae34e018aa
Anthony Avina <aavina2@gmail.com>
2019-10-20 13:19:58 -0500

    Created main content view of app
    - Added launch screen color based on lumpen blue
    - Added new font, Akkurat-Regular
    - Used SwiftUI to make view
    - Added constant variables for whatever needed it
    - Made button with radio image in it
    - Added background image
    - Button starts an alertview (for now)

--- Lumpen Radio/ContentView.swift
+++ Lumpen Radio/ContentView.swift
@@ -8,9 +8,68 @@
 
 import SwiftUI
 
+let CUSTOM_FONT = "Akkurate-Regular"
+let RADIO_IMAGE_NAME = "RadioButton"
+let RADIO_BUTTON_MIN: CGFloat = 100
+let RADIO_BUTTON_MAX_IDEAL: CGFloat = 250
+let OPACITY_FACTOR = 0.77
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        ZStack {
+            Image("Background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+                .aspectRatio(contentMode: .fill)
+            MainContent()
+        }
+    }
+}
+
+struct MainContent: View {
+    var body: some View {
+        VStack(alignment: .center, spacing: 16.0) {
+            Text("LUMPEN RADIO")
+                .font(.custom(CUSTOM_FONT, size: 45))
+                .fontWeight(.heavy)
+                .foregroundColor(.white)
+            Text("WLPN 105.5 FM Chicago")
+                .font(.custom(CUSTOM_FONT, size: 26))
+                .foregroundColor(.white)
+            HStack {
+                RadioButton()
+            }
+            Text("Tap above to tune in.")
+                .font(.custom(CUSTOM_FONT, size: 22))
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .opacity(OPACITY_FACTOR)
+        }.padding()
+    }
+}
+
+struct RadioButton: View {
+    @State private var showingRadioAlert = false
+    
+    var body: some View {
+        Button(action: {
+            self.showingRadioAlert = true
+        }) {
+            Image(RADIO_IMAGE_NAME)
+            .resizable()
+            .frame(minWidth: RADIO_BUTTON_MIN,
+                   idealWidth: RADIO_BUTTON_MAX_IDEAL,
+                   maxWidth: RADIO_BUTTON_MAX_IDEAL,
+                   minHeight: RADIO_BUTTON_MIN,
+                   idealHeight: RADIO_BUTTON_MAX_IDEAL,
+                   maxHeight: RADIO_BUTTON_MAX_IDEAL,
+               alignment: .center)
+        }
+        .background(Color.white)
+        .opacity(OPACITY_FACTOR)
+        .alert(isPresented: $showingRadioAlert) {
+            Alert(title: Text("Nice"), message: Text("You clicked on the radio!"), dismissButton: .default(Text("Dismiss")))
+        }
     }
 }
 
