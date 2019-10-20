bad8d995b4a1e88c1fc27e40c69eb9c2107e523c
Anthony Avina <aavina2@gmail.com>
2019-10-20 13:04:46 -0500

    initial commit

---
+++ Lumpen Radio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lumpen Radio
+//
+//  Created by Anthony on 10/20/19.
+//  Copyright © 2019 Public Media Institute. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
