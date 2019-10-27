bf63b023dd5a4880a5bbc09a902ba2aced1cafd9
Tai Ho <tai.ho@snapsheet.me>
2019-10-26 20:43:35 -0500

    video background

--- Lumpen Radio/MainViewController.swift
+++ Lumpen Radio/MainViewController.swift
@@ -7,17 +7,37 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class MainViewController: UIViewController, RadioDelegate {
 
     private var radio: Radio?
+    private var player: AVPlayer?
+    
     @IBOutlet weak var radioSubtext: UILabel!
+    
+    deinit {
+        unregisterAppLifeCycleEvents()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         radio = Radio(self)
         radio?.setupAudioSession()
+        initializeVideoPlayerWithVideo()
+        registerAppLifeCycleEvents()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.player?.play()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        self.player?.pause()
     }
     
     @IBAction func radioButtonTapped(_ sender: Any) {
@@ -28,14 +48,53 @@ class MainViewController: UIViewController, RadioDelegate {
         radioSubtext.text = textContent
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func registerAppLifeCycleEvents() {
+        NotificationCenter.default.addObserver(self, selector: #selector(pauseVideo), name: UIApplication.willResignActiveNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(playVideo), name: UIApplication.didBecomeActiveNotification, object: nil)
     }
-    */
+    
+    private func unregisterAppLifeCycleEvents() {
+        NotificationCenter.default.removeObserver(self, name: UIApplication.willResignActiveNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIApplication.didBecomeActiveNotification, object: nil)
+    }
+    
+    @objc private func pauseVideo() {
+        self.player?.pause()
+    }
+    
+    @objc private func playVideo() {
+        self.player?.play()
+    }
+    
+    private func initializeVideoPlayerWithVideo() {
 
+        // get the path string for the video from assets
+        let videoString:String? = Bundle.main.path(forResource: "turntable-loop-1920x500-h264-512kbps-h264", ofType: "mp4")
+        guard let unwrappedVideoPath = videoString else {return}
+
+        // convert the path string to a url
+        let videoUrl = URL(fileURLWithPath: unwrappedVideoPath)
+
+        // initialize the video player with the url
+        self.player = AVPlayer(url: videoUrl)
+
+        // create a video layer for the player
+        let layer: AVPlayerLayer = AVPlayerLayer(player: player)
+
+        // make the layer the same size as the container view
+        layer.frame = self.view.bounds
+
+        // make the video fill the layer as much as possible while keeping its aspect size
+        layer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+
+        // add the layer to the container
+        self.view.layer.insertSublayer(layer, at: 0)
+        
+        // play video
+        NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime, object: player?.currentItem, queue: .main) { [weak self] _ in
+            guard let `self` = self else {return}
+            self.player?.seek(to: CMTime.zero)
+            self.player?.play()
+        }
+    }
 }
